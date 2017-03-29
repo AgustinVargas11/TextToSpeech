1ce9adbf1d89061f786bd82639cc99f127c84fcc
Agustin Vargas <agustinvargas11@gmail.com>
2017-03-28 20:25:24 -0400

    gh-pages fix

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,87 +1,83 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app')
-        .controller('MainController', mainController);
+  angular
+    .module('app')
+    .controller('MainController', mainController);
 
-    mainController.$inject = ['$scope', '$log', '$timeout', 'SpeakService'];
+  mainController.$inject = ['$scope', '$log', '$timeout', 'SpeakService'];
 
-    function mainController($scope, $log, $timeout, SpeakService) {
+  function mainController($scope, $log, $timeout, SpeakService) {
 
-        const vm = this;
+    const vm = this;
 
-        init('Welcome', function () {
-            $log.info('Ready...');
-            // spinner exit was too abrupt
-            $timeout(function () {
-                vm.loading = false;
-            }, 300);
+    init('Welcome', function () {
+      $log.info('Ready...');
+      // spinner exit was too abrupt
+      $timeout(function () {
+        vm.loading = false;
+      }, 300);
+    });
+
+    $scope.$on('speech start', function () {
+      $scope.$apply(vm.buttonState = true)
+    });
+
+    $scope.$on('speech done', function () {
+      $scope.$apply(vm.buttonState = false);
+    });
+
+    function init(greeting, callback) {
+      vm.person = {};
+      vm.options = {};
+      vm.greeting = greeting || 'Hello';
+      vm.speak = speak;
+      vm.setName = setName;
+      vm.setVoice = setVoice;
+      vm.resetOptions = resetOptions;
+      
+      if (SpeakService.error) {
+        vm.loading = false;
+        vm.error = 'Please use a better browser that supports current APIs.';
+        return;
+      }
+
+      vm.setOptions = SpeakService.setOptions;
+
+      SpeakService.getVoices()
+        .then(function (voices) {
+          vm.voices = voices;
+          resetOptions();
         });
 
-        $scope.$on('speech start', function () {
-            $scope.$apply(vm.buttonState = true)
-        });
-
-        $scope.$on('speech done', function () {
-            $scope.$apply(vm.buttonState = false);
-        });
-
-        function init(greeting, callback) {
-            vm.person = {};
-            vm.options = {};
-            vm.greeting = greeting || 'Hello';
-            vm.speak = speak;
-            vm.setName = setName;
-            vm.setVoice = setVoice;
-            vm.resetOptions = resetOptions;
-
-          console.log('bar');
-          if (SpeakService.error) {
-            vm.loading = false;
-            vm.error = 'Please use a better browser that supports current APIs.';
-            return;
-          }
-
-          vm.setOptions = SpeakService.setOptions;
-
-            SpeakService.getVoices()
-                .then(function (voices) {
-                    vm.voices = voices;
-                    resetOptions();
-                });
-
-            if (typeof callback === 'function') {
-                callback();
-            }
-        }
-
-        function setName(name, formObj) {
-            if (!name) {
-                return formObj.nameInput.$setValidity("valid", false);
-            }
-            vm.person.name = name;
-        }
-
-        function speak(delay) {
-            console.log(delay);
-            SpeakService.setOptions(vm.options);
-            $timeout(SpeakService.speak, delay);
-        }
-
-        function setVoice(voice) {
-            vm.options.voice = voice;
-        }
-
-      console.log('foo');
-
-      function resetOptions() {
-            vm.options = {
-                volume: 1,
-                text: '',
-                rate: 1,
-                pitch: 1
-            };
-        }
+      if (typeof callback === 'function') {
+        callback();
+      }
     }
+
+    function setName(name, formObj) {
+      if (!name) {
+        return formObj.nameInput.$setValidity("valid", false);
+      }
+      vm.person.name = name;
+    }
+
+    function speak(delay) {
+      SpeakService.setOptions(vm.options);
+      $timeout(SpeakService.speak, delay);
+    }
+
+    function setVoice(voice) {
+      vm.options.voice = voice;
+    }
+
+    function resetOptions() {
+      vm.options = {
+        volume: 1,
+        text: '',
+        rate: 1,
+        pitch: 1
+      };
+    }
+  }
 })();
